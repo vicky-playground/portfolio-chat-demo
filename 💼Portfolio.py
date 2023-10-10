e0299ace3c1816f06f05376d59724d6e6b9088d4
Vicky <90204593+vicky-playground@users.noreply.github.com>
2023-10-10 10:46:51 -0400

    Update 💼Portfolio.py

--- 💼Portfolio.py
+++ 💼Portfolio.py
@@ -54,13 +54,12 @@ import streamlit.components.v1 as components
 import streamlit as st
 import streamlit.components.v1 as components
 
-def ChangeButtonColour(widget_label, font_color, background_color='transparent'):
+def ChangeButtonColour(widget_label, background_color='transparent'):
     htmlstr = f"""
         <script>
             var elements = window.parent.document.querySelectorAll('button');
             for (var i = 0; i < elements.length; ++i) {{ 
                 if (elements[i].innerText == '{widget_label}') {{ 
-                    elements[i].style.color ='{font_color}';
                     elements[i].style.background = '{background_color}'
                 }}
             }}
@@ -74,8 +73,8 @@ cols[1].button('second button', key='b2')
 cols[2].button('third button', key='b3')
 cols[3].button('fourth button', key='b4')
 
-ChangeButtonColour('second button', 'red', 'blue') # button txt to find, colour to assign
-ChangeButtonColour('fourth button', '#c19af5', '#354b75') # button txt to find, colour to assign
+ChangeButtonColour('second button', 'blue') # button txt to find, colour to assign
+ChangeButtonColour('fourth button', '#354b75') # button txt to find, colour to assign
 
 
 with col1:
